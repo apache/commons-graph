b367624f83452edb89c25914c7b8c5b2293b3f86
Simone Tripodi <simonetripodi@apache.org>
2011-06-11 20:33:03 +0000

    WeightedEdge implementations have to be comparable

--- src/main/java/org/apache/commons/graph/WeightedEdge.java
+++ src/main/java/org/apache/commons/graph/WeightedEdge.java
@@ -26,7 +26,7 @@ package org.apache.commons.graph;
  * @param <W> the {@link Edge} weight.
  */
 public interface WeightedEdge<W extends Number>
-    extends Edge
+    extends Edge, Comparable<WeightedEdge<Number>>
 {
 
     /**
