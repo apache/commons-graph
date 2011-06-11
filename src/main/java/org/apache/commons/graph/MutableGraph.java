880dd84659a6f98f56ffa6087f325b046e2ce476
Simone Tripodi <simonetripodi@apache.org>
2011-06-11 17:03:10 +0000

    organized imports

--- src/main/java/org/apache/commons/graph/MutableGraph.java
+++ src/main/java/org/apache/commons/graph/MutableGraph.java
@@ -19,7 +19,7 @@ package org.apache.commons.graph;
  * under the License.
  */
 
-import org.apache.commons.graph.exception.*;
+import org.apache.commons.graph.exception.GraphException;
 
 /**
  * The {@code MutableGraph} is a graph that supports the addition and removal of {@link Vertex} and {@link Edge}s.
