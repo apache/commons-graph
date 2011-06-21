f2f8df02b659d80f24aa17eae4831bb21fe332ba
Simone Tripodi <simonetripodi@apache.org>
2011-06-21 15:18:04 +0000

    minor format

--- src/main/java/org/apache/commons/graph/shortestpath/PredecessorsList.java
+++ src/main/java/org/apache/commons/graph/shortestpath/PredecessorsList.java
@@ -63,8 +63,7 @@ final class PredecessorsList<V extends Vertex, WE extends WeightedEdge<V>>
      */
     public WeightedPath<V, WE> buildPath( V source, V target, Double cost )
     {
-        InMemoryWeightedPath<V, WE> path =
-            new InMemoryWeightedPath<V, WE>( source, target, cost );
+        InMemoryWeightedPath<V, WE> path = new InMemoryWeightedPath<V, WE>( source, target, cost );
 
         V vertex = target;
         while ( !source.equals( vertex ) )
