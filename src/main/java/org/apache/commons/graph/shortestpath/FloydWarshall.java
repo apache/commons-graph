3cd3fadd18381c504c93dbf8a6d056951059a04f
Simone Tripodi <simonetripodi@apache.org>
2011-06-21 16:15:53 +0000

    first checkin of Bellman–Ford and Floyd–Warshall algorithms (empty stubs)

---
+++ src/main/java/org/apache/commons/graph/shortestpath/FloydWarshall.java
@@ -0,0 +1,58 @@
+package org.apache.commons.graph.shortestpath;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.commons.graph.Vertex;
+import org.apache.commons.graph.WeightedEdge;
+import org.apache.commons.graph.WeightedGraph;
+import org.apache.commons.graph.WeightedPath;
+
+/**
+ * Contains the Floyd–Warshall's shortest path algorithm implementation.
+ */
+public final class FloydWarshall
+{
+
+    /**
+     * This class can not be instantiated directly
+     */
+    private FloydWarshall()
+    {
+        // do nothing
+    }
+
+    /**
+     * Applies the classical Floyd–Warshall's algorithm to find the shortest path from the source to the target, if exists.
+     *
+     * @param <V> the Graph vertices type.
+     * @param <WE> the Graph weighted edges type
+     * @param graph the Graph which shortest path from {@code source} to {@code target} has to be found
+     * @param source the shortest path source Vertex
+     * @param target the shortest path target Vertex
+     * @return a path wich describes the shortes path, if any, otherwise a {@link PathNotFoundException} will be thrown
+     */
+    public static <V extends Vertex, WE extends WeightedEdge<V>> WeightedPath<V, WE> findShortestPath( WeightedGraph<V, WE> graph,
+                                                                                                       V source,
+                                                                                                       V target )
+    {
+        return null;
+    }
+
+}
