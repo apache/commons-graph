7a241fe32d351bd2f20ff2799d263aefaf57c757
Simone Tripodi <simonetripodi@apache.org>
2011-06-21 20:43:59 +0000

    first checkin of Heuristic interface, it will be used to calculate the h(x) function in A* algorithm

---
+++ src/main/java/org/apache/commons/graph/shortestpath/Heuristic.java
@@ -0,0 +1,42 @@
+package org.apache.commons.graph.shortestpath;
+
+import org.apache.commons.graph.Vertex;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/**
+ * An {@link Heuristic} instance is an admissible "heuristic estimate" of the distance to the goal
+ * (usually denoted <i>h(x)</i>).
+ *
+ * @param <V> the Graph vertices type.
+ */
+public interface Heuristic<V extends Vertex>
+{
+
+    /**
+     * Applies the <i>h(x)</i> "heuristic estimate" of the distance from the start to the goal.
+     *
+     * @param current the current visited Vertex in the Graph.
+     * @param goal the goal of the Graph visit.
+     * @return the "heuristic estimate" of the distance from the start to the goal.
+     */
+    Double applyHeuristic( V current, V goal );
+
+}
