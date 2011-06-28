c7a9e5974ed007067732dbd73c627d1e517f7250
Simone Tripodi <simonetripodi@apache.org>
2011-06-28 13:13:10 +0000

    the comparator returns -1 even when the comparable are equals, easier inverting the compare order

--- src/main/java/org/apache/commons/graph/coloring/GraphColoring.java
+++ src/main/java/org/apache/commons/graph/coloring/GraphColoring.java
@@ -67,7 +67,7 @@ public final class GraphColoring
         {
             public int compare( Integer o1, Integer o2 )
             {
-                return o1.compareTo( o2 ) * -1;
+                return o2.compareTo( o1 );
             }
         } );
 
