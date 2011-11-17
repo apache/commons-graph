35a38a8cab8aa042f75dc1d9f1d666bef8eeff97
Simone Tripodi <simonetripodi@apache.org>
2011-11-17 17:36:54 +0000

    first checkin of Tarjan's algorithm implementation

--- src/main/java/org/apache/commons/graph/connectedcomponents/Tarjan.java
+++ src/main/java/org/apache/commons/graph/connectedcomponents/Tarjan.java
@@ -19,9 +19,101 @@ package org.apache.commons.graph.connectedcomponents;
  * under the License.
  */
 
+import static java.lang.Math.min;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.Stack;
+
+import org.apache.commons.graph.DirectedGraph;
+import org.apache.commons.graph.Edge;
+import org.apache.commons.graph.Vertex;
+
+/**
+ * Tarjan's algorithm is a variation (slightly faster) on {@link KosarajuSharir}'s algorithm for finding
+ * strongly-connected components in a directed graph.
+ */
 public final class Tarjan
 {
 
-    // TODO
+    /**
+     * Applies the classical Tarjan's algorithm to find the strongly connected components, if exist.
+     *
+     * @param <V> the Graph vertices type.
+     * @param <E> the Graph edges type.
+     * @param graph the Graph which strongly connected component has to be verified.
+     * @return
+     */
+    public static <V extends Vertex, E extends Edge> Set<V> getStronglyConnectedComponent( DirectedGraph<V, E> graph )
+    {
+        final Map<V, TarjanVertexMetaInfo> verticesMetaInfo = new HashMap<V, TarjanVertexMetaInfo>();
+        final Stack<V> s = new Stack<V>();
+        final Set<V> stronglyConnectedComponent = new HashSet<V>();
+        Integer index = 0;
+
+        for ( V vertex : graph.getVertices() )
+        {
+            TarjanVertexMetaInfo vertexMetaInfo = getMetaInfo( vertex, verticesMetaInfo );
+
+            if ( vertexMetaInfo.hasUndefinedIndex() )
+            {
+                strongConnect( graph, vertex, verticesMetaInfo, s, stronglyConnectedComponent, index );
+            }
+        }
+
+        return stronglyConnectedComponent;
+    }
+
+    private static <V> TarjanVertexMetaInfo getMetaInfo( V vertex, Map<V, TarjanVertexMetaInfo> verticesMetaInfo )
+    {
+        TarjanVertexMetaInfo vertexMetaInfo = verticesMetaInfo.get( vertex );
+        if ( vertexMetaInfo == null )
+        {
+            vertexMetaInfo = new TarjanVertexMetaInfo();
+            verticesMetaInfo.put( vertex, vertexMetaInfo );
+        }
+        return vertexMetaInfo;
+    }
+
+    private static <V extends Vertex, E extends Edge> void strongConnect( DirectedGraph<V, E> graph,
+                                                                          V vertex,
+                                                                          Map<V, TarjanVertexMetaInfo> verticesMetaInfo,
+                                                                          Stack<V> s,
+                                                                          Set<V> stronglyConnectedComponent,
+                                                                          Integer index )
+    {
+        TarjanVertexMetaInfo vertexMetaInfo = getMetaInfo( vertex, verticesMetaInfo );
+        vertexMetaInfo.setIndex( index );
+        vertexMetaInfo.setLowLink( index );
+        index++;
+        s.push( vertex );
+
+        for ( V adjacent : graph.getOutbound( vertex ) )
+        {
+            TarjanVertexMetaInfo adjacentMetaInfo = getMetaInfo( adjacent, verticesMetaInfo );
+            if ( adjacentMetaInfo.hasUndefinedIndex() )
+            {
+                strongConnect( graph, adjacent, verticesMetaInfo, s, stronglyConnectedComponent, index );
+                vertexMetaInfo.setLowLink( min( vertexMetaInfo.getLowLink(), adjacentMetaInfo.getLowLink() ) );
+            }
+            else if ( s.contains( adjacent ) )
+            {
+                vertexMetaInfo.setLowLink( min( vertexMetaInfo.getLowLink(), adjacentMetaInfo.getIndex() ) );
+            }
+        }
+
+        if ( vertexMetaInfo.getLowLink() == vertexMetaInfo.getIndex() )
+        {
+            V v;
+            do
+            {
+                v = s.pop();
+                stronglyConnectedComponent.add( v );
+            }
+            while ( !vertex.equals( v ) );
+        }
+    }
 
 }
