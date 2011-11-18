6b0278d447d0c42d947594123d794e6628e87844
Simone Tripodi <simonetripodi@apache.org>
2011-11-18 16:23:02 +0000

    first checkin of CheriyanMehlhornGabowVisitHandler class

---
+++ src/main/java/org/apache/commons/graph/ssc/CheriyanMehlhornGabowVisitHandler.java
@@ -0,0 +1,37 @@
+package org.apache.commons.graph.ssc;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.commons.graph.Edge;
+import org.apache.commons.graph.Vertex;
+import org.apache.commons.graph.visit.BaseGraphVisitHandler;
+
+/**
+ * Cheriyan/Mehlhorn/Gabow's strongly connected component algorithm is based on DFS (or BFS) algorithm
+ * and needs to execute specific actions during the visit.
+ *
+ * @param <V> the Graph vertices type.
+ * @param <E> the Graph edges type.
+ */
+final class CheriyanMehlhornGabowVisitHandler<V extends Vertex, E extends Edge>
+    extends BaseGraphVisitHandler<V, E>
+{
+
+}
