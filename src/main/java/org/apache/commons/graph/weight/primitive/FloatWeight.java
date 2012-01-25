892d27abb3c7501c902f751192dcabf82520d622
Simone Tripodi <simonetripodi@apache.org>
2012-01-25 15:23:38 +0000

    more predefined weights on top of Number implementations

---
+++ src/main/java/org/apache/commons/graph/weight/primitive/FloatWeight.java
@@ -0,0 +1,68 @@
+package org.apache.commons.graph.weight.primitive;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.commons.graph.weight.OrderedMonoid;
+
+/**
+ * A {@link FloatWeight} provides operations and properties
+ * for weights of type {@link Float}.
+ */
+public class FloatWeight
+    implements OrderedMonoid<Float>
+{
+
+    /**
+     * {@inheritDoc}
+     */
+    public Float zero()
+    {
+        return 0.0F;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Float append( Float s1, Float s2 )
+    {
+        if ( s1 == null || s2 == null )
+        {
+            return null;
+        }
+        return s1 + s2;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Float inverse( Float element )
+    {
+        return -element;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public int compare( Float s1, Float s2 )
+    {
+        return s1.compareTo( s2 );
+    }
+
+}
