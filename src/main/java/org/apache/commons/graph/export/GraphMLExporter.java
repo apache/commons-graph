3dbe499672dc83322bac65325112ace0132b4896
Simone Tripodi <simonetripodi@apache.org>
2012-03-19 22:02:06 +0000

    code format

--- src/main/java/org/apache/commons/graph/export/GraphMLExporter.java
+++ src/main/java/org/apache/commons/graph/export/GraphMLExporter.java
@@ -178,31 +178,32 @@ final class GraphMLExporter<V, E>
         return STRING;
     }
 
-	@Override
-	public <N extends Number> GraphMLExporter<V, E>
-	    withEdgeWeights(Mapper<E, N> edgeWeights) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public <N extends Number> GraphMLExporter<V, E> withEdgeWeights( Mapper<E, N> edgeWeights )
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public <N extends Number> GraphMLExporter<V, E> withVertexWeights(
-			Mapper<V, N> vertexWeights) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public <N extends Number> GraphMLExporter<V, E> withVertexWeights( Mapper<V, N> vertexWeights )
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public GraphMLExporter<V, E> withEdgeLabels(Mapper<E, String> edgeLabels) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public GraphMLExporter<V, E> withEdgeLabels( Mapper<E, String> edgeLabels )
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public GraphMLExporter<V, E> withVertexLabels(
-			Mapper<V, String> vertexLabels) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public GraphMLExporter<V, E> withVertexLabels( Mapper<V, String> vertexLabels )
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
