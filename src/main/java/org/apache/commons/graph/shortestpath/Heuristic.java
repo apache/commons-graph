03ec5dc8f6b897be8f4f3ee8b212c29449322ecc
Marco Speranza <marcosperanza@apache.org>
2012-07-12 08:00:59 +0000

    Dropped unnecessary empty lines, no functional modifications

--- src/main/java/org/apache/commons/graph/shortestpath/Heuristic.java
+++ src/main/java/org/apache/commons/graph/shortestpath/Heuristic.java
@@ -1,6 +1,5 @@
 package org.apache.commons.graph.shortestpath;
 
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
